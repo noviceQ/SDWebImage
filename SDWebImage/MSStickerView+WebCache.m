d6dc21054890e707d4c7f9923e7e65cb42f493ea
ZXQiao <xiuqiao.zhang@xinmei365.com>
2016-10-05 09:34:08 +0800

    change to dispatch_asyb_safe

--- SDWebImage/MSStickerView+WebCache.m
+++ SDWebImage/MSStickerView+WebCache.m
@@ -37,7 +37,7 @@ static char TAG_ACTIVITY_SHOW;
         id <SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadImageWithURL:url options:options progress:progressBlock completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
             [wself removeActivityIndicator];
             if (!wself) return;
-            dispatch_main_sync_safe(^{
+            dispatch_main_async_safe(^{
                 if (!wself) return;
                 if (image && (options & SDWebImageAvoidAutoSetImage) && completedBlock)
                 {
